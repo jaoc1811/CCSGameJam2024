32a5a6f7436226732cce20c008051bde11df5624
Juan Oropeza <jaoc1811@gmail.com>
2024-01-28 10:14:42 -0400

    added sounds to typing scene

--- Assets/Scripts/Minigames/Typing.cs
+++ Assets/Scripts/Minigames/Typing.cs
@@ -14,10 +14,14 @@ public class Typing : MonoBehaviour
     [SerializeField] int wordLength = 5;
     [SerializeField] TMP_Text[] text;
     [SerializeField] int nextLetterIndex;
+    [SerializeField] AudioClip typeSound;
+    [SerializeField] AudioClip sendSound;
+    [SerializeField] AudioClip recieveSound;
     string laugh;
 
     void Start()
     {
+        GameManager.instance.StartStage();
         nextLetterIndex = 0;
         laugh = GenerateRandomString();
         Debug.Log(laugh);
@@ -34,11 +38,12 @@ public class Typing : MonoBehaviour
                 Debug.Log("Lose");
             }else {
                 text[nextLetterIndex].color = Color.black;
+                AudioSource.PlayClipAtPoint(typeSound, Camera.main.transform.position, 0.3f);
                 nextLetterIndex++;
             }
             
             if (nextLetterIndex == laugh.Length){
-                Debug.Log("Win");
+                WinStage();
             }
         }
     }
@@ -48,5 +53,16 @@ public class Typing : MonoBehaviour
         var chars = Enumerable.Range(0, wordLength)
             .Select(x => lettersPool[Random.Range(0, lettersPool.Length)]);
         return new string(chars.ToArray());
+    }
+
+    private void WinStage() {
+        GameManager.instance.WinStage();
+        StartCoroutine(SendReceive());
+    }
+
+    IEnumerator SendReceive() {
+        AudioSource.PlayClipAtPoint(sendSound, Camera.main.transform.position, 0.3f);
+        yield return new WaitForSeconds(0.5f);
+        AudioSource.PlayClipAtPoint(recieveSound, Camera.main.transform.position, 0.3f);
     }
 }
